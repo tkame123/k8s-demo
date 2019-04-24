fdb693d10ebf528652668aa07ba7293f1435b318
Takeshi Kameyama <tkame123@gmail.com>
2019-04-24 17:12:50 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          {/*<img src="/tk.jpeg" alt="tk" />*/}
+          <img src="/tk.jpeg" alt="tk" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
